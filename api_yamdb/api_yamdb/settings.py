d2f9e445f59d55924eb5ec8627bf4679cffe477c
Vitalino13 <vitaliiast93@gmail.com>
2023-04-12 10:57:16 +0300

    Dockerfile Commit

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY_DJANGO', default='somefakesecretkey')
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
